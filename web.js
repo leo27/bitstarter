e0709a637465290d93fea9bb7d414d794818212c
Leo Hernandez <leo@veintisiete.com.mx>
2013-07-10 00:56:46 +0000

    Added code to read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,9 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
-
+var fs = require('fs');
+var contentBuffer = fs.readFileSync('index.html');
 app.get('/', function(request, response) {
-  response.send('Hello World! 2');
+    response.send(contentBuffer.toString());
 });
 
 var port = process.env.PORT || 5000;
